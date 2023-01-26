4553fa274f968823686a5b3e878639adc29d5ed4
Saurabh Kapoor <saurabh.kapoor@lifecheq.co.za>
2023-01-26 12:47:45 +0530

    fix build paths

--- .github/build-linux.sh
+++ .github/build-linux.sh
@@ -1,12 +1,12 @@
-cd src
+pushd pug/src
 make clean
 # export CFLAGS="-static -DUSE_READLINE=1"
 CFLAGS="-DUSE_READLINE=1" LDFLAGS="-lm -ledit" make
 rm -rf pug
 mkdir -p pug
-cp gofer ./pug
+cp gofer ./pug/pug
 cp ../pusimple.pre ../pustd.pre ../pucc28.pre ./pug
 cd pug
 # Zip all files in the pug directory
 zip -r ../pug_linux.zip *
-cd ../..
+popd
