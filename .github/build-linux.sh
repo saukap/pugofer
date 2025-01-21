d886ffb93385b3eebb6cc729e0cf67a03291d15f
Saurabh Kapoor <saurabh.kapoor@lifecheq.co.za>
2025-01-21 16:27:37 +0530

    build pug app

--- .github/build-linux.sh
+++ .github/build-linux.sh
@@ -2,9 +2,14 @@ pushd pug/src
 make clean
 # export CFLAGS="-static -DUSE_READLINE=1"
 CFLAGS="-DUSE_READLINE=1" LDFLAGS="-lm -ledit" make
+CFLAGS="-DUSE_READLINE=1" LDFLAGS="-lm -ledit" make gofer_app
 rm -rf pug
 mkdir -p pug
+
 cp gofer ./pug/pug
+cp gofer_app ./pug/pug_app
+
+
 cp ../pusimple.pre ../pustd.pre ../pucc28.pre ./pug
 cd pug
 # Zip all files in the pug directory
